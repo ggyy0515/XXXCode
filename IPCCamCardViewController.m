71bc727e7cb4e54d8747f8e75956eb271d8e78dd
Tristan_yueng <5915762@qq.com>
2017-07-19 17:21:20 +0800

    code commit

--- IPCCamCardViewController.m
+++ IPCCamCardViewController.m
@@ -39,60 +39,60 @@
     // Do any additional setup after loading the view.
     self.navigationController.navigationBarHidden = NO;
     self.automaticallyAdjustsScrollViewInsets = NO;
-    self.view.backgroundColor = [UIColor yellowColor];
-//    [self.view addSubview:self.ipcCardView];
-//    @weakify(self);
-//    [self.ipcCardView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        @strongify(self);
-//        make.top.left.bottom.and.right.equalTo(self.view).with.insets(UIEdgeInsetsMake(0, 0, 0, 0));
-//        
-//    }];
-//    [self.ipcCardView refreshUI];
+    self.view.backgroundColor = [UIColor blackColor];
+    //    [self.view addSubview:self.ipcCardView];
+    //    @weakify(self);
+    //    [self.ipcCardView mas_makeConstraints:^(MASConstraintMaker *make) {
+    //        @strongify(self);
+    //        make.top.left.bottom.and.right.equalTo(self.view).with.insets(UIEdgeInsetsMake(0, 0, 0, 0));
+    //
+    //    }];
+    //    [self.ipcCardView refreshUI];
     
     if (!_timeView) {
         _timeView = [[DNVerticalTimeView alloc] initWithFrame:CGRectMake(0, SCREEN_HEIGHT - 350.f, SCREEN_WIDTH, 350.f)];
     }
     [self.view addSubview:_timeView];
-
+    
     CloudRecordModel *model1 = [[CloudRecordModel alloc] init];
     model1.chan_no = 1;
-    model1.start_time = 1499954400000 + 60*60*24*1000;//22
+    model1.start_time = 1499954400000 + 60*60*24*1000*6;//22
     model1.device_id = @"123";
     model1.time_len = 50000;
     model1.record_type = 2;
     
     CloudRecordModel *model2 = [[CloudRecordModel alloc] init];
     model2.chan_no = 1;
-    model2.start_time = 1499940000000 + 60*60*24*1000;//18
+    model2.start_time = 1499940000000 + 60*60*24*1000*6;//18
     model2.device_id = @"123";
     model2.time_len = 1050000;
     model2.record_type = 2;
     
     CloudRecordModel *model3 = [[CloudRecordModel alloc] init];
     model3.chan_no = 1;
-    model3.start_time = 1499932800000 + 60*60*24*1000;//16
+    model3.start_time = 1499932800000 + 60*60*24*1000*6;//16
     model3.device_id = @"123";
     model3.time_len = 550000;
     model3.record_type = 2;
     
     CloudRecordModel *model4 = [[CloudRecordModel alloc] init];
     model4.chan_no = 1;
-    model4.start_time = 1499922000000 + 60*60*24*1000;//13
+    model4.start_time = 1499922000000 + 60*60*24*1000*6;//13
     model4.device_id = @"123";
     model4.time_len = 850000;
     model4.record_type = 2;
     
     CloudRecordModel *model5 = [[CloudRecordModel alloc] init];
     model5.chan_no = 1;
-    model5.start_time = 1499904000000 + 60*60*24*1000;//8
+    model5.start_time = 1499904000000 + 60*60*24*1000*6;//8
     model5.device_id = @"123";
     model5.time_len = 950000;
     model5.record_type = 2;
     
     
     [_timeView setDatas:@[model1, model2, model3, model4, model5].mutableCopy startTime:0];
-//
-    [self performSelector:@selector(ggyy) withObject:nil afterDelay:3.f];
+    //
+    //    [self performSelector:@selector(ggyy) withObject:nil afterDelay:3.f];
 }
 
 - (void)ggyy {
@@ -134,19 +134,19 @@
 
 #pragma mark - pPSCamCardViewDelegate
 -(void)IPCCamCardView:(IPCCamCardView *)IPCCamCardView didSelectAction:(id)sender{
-
+    
     
 }
 
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
